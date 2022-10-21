f8a390ca71bfc81aaa7c6ff498adffca086dfba9
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2022-10-21 15:43:03 +0200

    Update BriscoApp.cpp

--- BriscoApp.cpp
+++ BriscoApp.cpp
@@ -26,7 +26,7 @@ bool BriscoApp::OnInit() {
     int loc;
     wxString s=wxFileName::GetPathSeparator();
     //wxString pathTraduzioni=wxPathOnly(argv[0])+s+"locale";
-	wxString path=wxT("/usr/share/wxBriscola");
+	wxString path=wxGetHomeDir();
     wxConfig *config=new wxConfig(GetAppName()); //lettura delle opzioni
 
     if (!config->Read("locale", &loc))
@@ -40,6 +40,7 @@ bool BriscoApp::OnInit() {
 #else
     wxLocale::AddCatalogLookupPathPrefix(wxT("/usr/local/share/locale"));
     wxLocale::AddCatalogLookupPathPrefix(wxT("/snap/wxbriscola/current/usr/local/share/locale"));
+    wxLocale::AddCatalogLookupPathPrefix(path+wxT("/.local/share/flatpak/app/org.altervista.numerone.wxbriscola/current/active/files/share/locale"));
 #endif //_WIN32
     if (!m_locale->AddCatalog("wxbriscola"))
        wxMessageBox(_("Impossibile trovare il catalogo del programma. Il programma si avviera' in italiano."), _("Attenzione"), wxICON_EXCLAMATION);
